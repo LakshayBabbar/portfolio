72ee37f9d10faad5f5be1c8e2e61af7f71a8a7ed
LakshayBabbar <lakshaybabbar0118@gmail.com>
2023-10-14 18:12:17 +0530

    Update(Beta) v5

--- src/components/skills/TopSkills.js
+++ src/components/skills/TopSkills.js
@@ -1,11 +1,14 @@
 import classes from './TopSkills.module.css';
 
 const TopSkills = (props) => {
+    const adjust = {
+        margin: `0 ${98 - props.progress}% 0 0`
+    };
     return (
             <div className={classes.wrapper} data-aos="zoom-in">
                 <div className={classes.details}>
                     <h5>{props.name}</h5>
-                    <span>{props.progress}%</span>
+                    <span style={adjust}>{props.progress}%</span>
                 </div>
                 <div className={classes.outer}>
                     <div className={classes.inner} style={{ width: `${props.progress}%` }}></div>
