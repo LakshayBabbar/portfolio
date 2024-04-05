4436ae3a88d32146fe5912af4314c6efbbb83852
LakshayBabbar <lakshaybabbar0118@gmail.com>
2024-04-05 22:35:06 +0530

    contact page optimisations

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,6 +5,7 @@ import { IoPhonePortraitOutline } from "react-icons/io5";
 import Socials from "@/components/ui/Socials";
 import { motion } from "framer-motion";
 import { useState, ChangeEvent, FormEvent } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
 const Contact = () => {
   const [data, setData] = useState({
@@ -12,9 +13,12 @@ const Contact = () => {
     email: "",
     message: "",
   });
+  const [loading, setLoading] = useState<boolean>(false);
+  const { toast } = useToast();
 
   const submitHandler = async (e: FormEvent) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const email = await fetch(`${process.env.NEXT_PUBLIC_CONTACT_API}`, {
         method: "POST",
@@ -27,7 +31,12 @@ const Contact = () => {
       const response = await email.json();
       console.log(response);
       setData({ name: "", email: "", message: "" });
-      alert("Message sent successfully.");
+      setLoading(false);
+      const date = new Date().toString();
+      toast({
+        title: "Your message is sent successfully.",
+        description: date,
+      });
     } catch (error) {
       console.error(error);
     }
@@ -44,69 +53,63 @@ const Contact = () => {
 
   return (
     <motion.div
-      className="h-screen flex flex-col mt-28 sm:mt-0 items-center justify-center bg-grid-white/[0.08]"
+      className="h-screen flex flex-col items-center justify-center bg-dot-white/[0.2]"
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ type: "spring" }}
     >
-      <section>
-        <h1 className="text-6xl font-bold mb-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 leading-tight">
-          Get In Touch
-        </h1>
-        <div className="flex gap-20 border p-20 border-neutral-500">
-          <form className="w-96 space-y-4" onSubmit={submitHandler}>
-            <input
-              type="text"
-              required
-              className="bg-transparent border-b border-neutral-500 h-10 rounded-md px-4 outline-none w-full"
-              placeholder="Name"
-              name="name"
-              value={data.name}
-              onChange={valueHandler}
-            />
-            <input
-              type="email"
-              required
-              name="email"
-              value={data.email}
-              className="bg-transparent border-b border-neutral-500 h-10 rounded-md px-4 outline-none w-full"
-              placeholder="Email"
-              onChange={valueHandler}
-            />
-            <textarea
-              className="h-16 max-h-60 bg-transparent border-b border-neutral-500 rounded-md p-4 outline-none w-full"
-              placeholder="Message"
-              value={data.message}
-              name="message"
-              onChange={valueHandler}
-              required
-            />
-
-            <Button
-              type="submit"
-              className="w-full rounded-full"
-              variant="secondary"
-            >
-              Submit
-            </Button>
-          </form>
-          <div>
-            <div className="space-y-2">
-              <h1 className="text-2xl mb-3">Contact</h1>
-              <p className="flex items-center">
-                <MdEmail />
-                &nbsp;Lakshay.babbar.1801@gmail.com
-              </p>
-              <p className="flex items-center">
-                <IoPhonePortraitOutline />
-                &nbsp;9953712825
-              </p>
-            </div>
-            <div className="mt-[6.5rem]">
-              <Socials hidden={false} />
-            </div>
+      <section className="px-5 w-full sm:w-4/5 md:w-[45rem]">
+        <div className="flex flex-col gap-2 items-center mb-10">
+          <h1 className="text-4xl sm:text-6xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 leading-tight">
+            Get In Touch
+          </h1>
+          <p className="flex items-center">
+            <MdEmail />
+            &nbsp;Lakshay.babbar.1801@gmail.com
+          </p>
+          <p className="flex items-center">
+            <IoPhonePortraitOutline />
+            &nbsp;9953712825
+          </p>
+          <div className="mt-2">
+            <Socials hidden={false} />
           </div>
         </div>
+        <form className="space-y-8" onSubmit={submitHandler}>
+          <input
+            type="text"
+            required
+            className="bg-transparent border-b border-white h-10 px-4 outline-none w-full"
+            placeholder="Name"
+            name="name"
+            value={data.name}
+            onChange={valueHandler}
+          />
+          <input
+            type="email"
+            required
+            name="email"
+            value={data.email}
+            className="bg-transparent border-b border-white h-10 px-4 outline-none w-full"
+            placeholder="Email"
+            onChange={valueHandler}
+          />
+          <textarea
+            className="h-28 max-h-60 bg-transparent border-b border-white px-4 outline-none w-full"
+            placeholder="Message"
+            value={data.message}
+            name="message"
+            onChange={valueHandler}
+            required
+          />
+          <Button
+            type="submit"
+            className="w-28 rounded-none"
+            disabled={loading}
+          >
+            Send
+          </Button>
+        </form>
       </section>
     </motion.div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,12 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${inter.className} dark:bg-black/[0.96]`}
-      >
+      <body className={`${inter.className} dark:bg-black/[0.96]`}>
         <Navbar />
         {children}
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
